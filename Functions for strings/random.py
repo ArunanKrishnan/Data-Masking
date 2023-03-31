68365f03f5e2f710dd8118c2fec4bfdacc208cb7
Sree Gayathri <ethirajsreegayathri@gmail.com>
2023-03-31 14:55:23 +0530

    add

--- Functions for strings/random.py
+++ Functions for strings/random.py
@@ -1,9 +1,14 @@
 from mimesis import Person
 import pandas as pd
-input_file = r'C:\Users\sethir919\Desktop\masking project\Data-Obfuscation-1\masking-input.xlsx'
+input_file = r'C:\Users\sethir919\Desktop\project\masking-input.xlsx'
 data = pd.read_excel(input_file)
 person = Person()
 fake_names = [person.full_name() for i in range(10)]
+
 data = pd.DataFrame({'Name': fake_names})
-data.to_excel('output_file.xlsx', index=False)
+data.to_excel('output_file_random.xlsx', index=False)
 print(data)
+
+
+
+
