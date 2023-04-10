0278bfceefdf14bd9547bd4676d922aa4f733e58
ArunanKrishnan <124554447+ArunanKrishnan@users.noreply.github.com>
2023-04-10 11:01:52 +0530

    Add files via upload

---
+++ shuffling the indexes.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import random
+
+# This method shuffles the the indexes of the paticular data in that column
+
+file_path = r'C:\Users\akrish451\Desktop\datamaskingproject\Data-Obfuscation-1\input files\masking-input.xlsx'
+df = pd.read_excel(file_path)
+column_name = "Employee ID"  
+column_values = df[column_name]
+result_values = []
+
+# creating the loop for creating a list for each digit and shuffling it
+
+for value in column_values:
+    
+    digits_list = [int(digit) for digit in str(value)]
+    
+    
+    random.shuffle(digits_list)
+    
+    # combining the digits and appending to the list
+
+    result_value = int("".join(map(str, digits_list)))
+    
+    
+    result_values.append(result_value)
+
+
+print("Original values:", column_values.tolist())
+print("Result values:", result_values)
