780204b32ca7074c1e46e9dc7d3fa77dee3562db
Sree Gayathri <ethirajsreegayathri@gmail.com>
2023-04-17 07:11:51 +0530

    created new file

--- float_masking.py
+++ float_masking.py
@@ -1,14 +1,18 @@
 import pandas as pd
-import random
-import string
 import struct
 import numpy as np
 # Load the input Excel file
-input_file = r'C:\Users\sethir919\Desktop\project\masking-input.xlsx'
+input_file = r'C:\Users\sethir919\Desktop\masking project\Data-Obfuscation\data\masking-input.xlsx'
 data = pd.read_excel(input_file)
 
+def chain(start, *funcs):
+    res = start
+    for func in funcs:
+        res = func(res)
+    return res
+
 # Define a function to mask a single name
-def mask_name(weight):
+def mask_float_by_str(weight): #56.64
     samp=str(weight)
     one = samp[0]
     two=samp[1]
@@ -16,22 +20,10 @@ def mask_name(weight):
     four= samp[-1]
     masked_name = three+four+"."+one+two
     return  masked_name 
+#data['Weight']=data['Weight'].apply(mask_float_by_str)
+#excel=print(data['Weight'][0])
 
-
-def mask_float_values(input_list, mask_char='*', mask_start=0, mask_end=None):
-    """
-    Mask float values in input_list with mask_char from mask_start to mask_end indices.
-    If mask_end is not provided, it defaults to the end of the input_list.
-    """
-    if mask_end is None:
-        mask_end = len(input_list)
-    masked_list = input_list.copy()
-    for i in range(mask_start, mask_end):
-        if isinstance(masked_list[i], float):
-            masked_list[i] = mask_char
-    return masked_list
-
-def mask_float(value):#3,.5
+def mask_float_by_precision(weight):
     """
     Mask a float value by replacing the last `precision` digits with `mask_value`.
     :param value: the float value to be masked
@@ -39,18 +31,21 @@ def mask_float(value):#3,.5
     :param precision: the number of digits to be masked
     :return: the masked float value
     """
-    precision=8
-    factor = 10 * precision #20 
-    mask_value=7*int(value * factor)
-    masked_value = int(value * factor) # get the integer part before the decimal
+    precision=3
+    factor = 2 * precision #20 
+    mask_value=7*int(weight * factor)
+    masked_value = int(weight * factor) # get the integer part before the decimal
     rand=str(mask_value) * precision
     masked_value = round(float(str(masked_value) + '.' + rand),2) # append the mask_value
-    return masked_value%100
+    res=masked_value%100
+    return res
+#data['Weight']=data['Weight'].apply(mask_float_by_precision)
+#print(data['Weight'])
 
 # define two floating-point numbers
-def mask_val(val):
-    a =76.89
-    b =val
+def mask_float_by_add(weight):
+    a =weight
+    b =3.14
     # convert the numbers to their IEEE 754 binary representation
     a_bin = struct.pack('!f', a)
     b_bin = struct.pack('!f', b)
@@ -60,22 +55,27 @@ def mask_val(val):
 # convert the result back to a floating-point number
     result = struct.unpack('!f', result_bin)[0]
     return result
+#data['Weight']=data['Weight'].apply(mask_float_by_add)
 
 
-def mask_or(val):
+def mask_float_by_or(weight):
     a =150
-    b = 200
+    b =weight
     # convert the numbers to their IEEE 754 binary representation
     a_bin = struct.pack('!f', a)
     b_bin = struct.pack('!f', b)
-    # perform a bitwise AND operation on the binary strings
+    # perform a bitwise OR operation on the binary strings
 
     result_bin = bytearray([x | y for x, y in zip(a_bin, b_bin)])
 # convert the result back to a floating-point number
     result = struct.unpack('!f', result_bin)[0]
     return result
+#data['Weight']=data['Weight'].apply(mask_float_by_or)
 
-def float_bit_shift_and_mask(x, shift_amount, mask):
+def mask_float_by_shift(x):
+    # Define the shift amount and mask
+    shift_amount = 1
+    mask = 1111000011000
     # Convert the floating-point number to its IEEE 754 binary representation
     x_bin = bytearray(struct.pack('!f', x))
 
@@ -90,15 +90,9 @@ def float_bit_shift_and_mask(x, shift_amount, mask):
 
     # Return the resulting floating-point number
     return result
+#data['Weight']=data['Weight'].apply(mask_float_by_shift)
 
-# Define the shift amount and mask
-shift_amount = 1
-mask = 1111000011000
-
-#data['Weight'] = data['Weight'].apply(float_bit_shift_and_mask, shift_amount=shift_amount, mask=mask)
-#print(data['Weight'])
-
-def float_rotate(x):
+def mask_float_by_rotate(x):
     r=150
     # Convert float to binary string
     bits = bin(struct.unpack('!I', struct.pack('!f', x))[0])
@@ -109,10 +103,11 @@ def float_rotate(x):
     # Convert binary string back to float
     x_rotated = struct.unpack('!f', struct.pack('!I', int(bits, 2)))[0]
     return round(x_rotated,2)
-#data['Weight']=data['Weight'].apply(float_rotate)
+#data['Weight']=data['Weight'].apply(mask_float_by_rotate)
 #print(data['Weight'])
 
-def swap_floats_one(a,b):
+def mask_float_by_swap1(a):
+    b=77
     """
     Swaps the integer parts of two float values and returns them as a tuple.
     """
@@ -120,42 +115,16 @@ def swap_floats_one(a,b):
     b_int, b_dec = divmod(b, 2)
     return b_int + a_dec
 # Apply the swap_floats() function to the 'Weight' column
-# data['Weight'] = data['Weight'].apply(swap_floats_one,b=77)
+#data['Weight'] = data['Weight'].apply(mask_float_by_swap1)
 
 
-def swap_floats(a):
+def mask_float_by_swap2(weight):
     """
     Swaps the integer parts of two float values and returns them as a tuple.
     """
-    a_int, a_dec = divmod(a, 2)
+    a_int, a_dec = divmod(weight, 2)
     return a_dec +a_int
-# Apply the swap_floats() function to the 'Weight' column
-#data['Weight'] = data['Weight'].apply(swap_floats)
-
-
-'''def shuffle_column(weight):
-    shuffled = np.random.permutation(weight)
-    return shuffled
-data['Weight'] = data['Weight'].apply(shuffle_column)
-print(data['Weight'])'''
-import pandas as pd
-import numpy as np
-
-def shuffle_column_values(df, column_name):
-    # shuffle the values in the specified column
-    shuffled_column = df[column_name].sample(frac=1).reset_index(drop=True)
-    # create a new DataFrame with shuffled column
-    shuffled_df = pd.DataFrame({
-        column_name: shuffled_column})
-    return shuffled_df
-shuffled_weight = shuffle_column_values(data, 'Weight')
-print(shuffled_weight)
-
-
-
-
-
-
+#data['Weight']=data['Weight'].apply(mask_float_by_swap2)
 
 
 
---
+++ varchar_masking.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import hashlib
+import os
+import random
+
+#import input data
+data = pd.read_excel(r'C:\Users\kramal361\Desktop\datamaskingproject\Data-Obfuscation-2\masking-input.xlsx')
+def shuffle_email(email):
+    '''Shuffles the local-part of an email address using a random shuffling algorithm.
+    Args:
+        email (str): The email address to be shuffled.
+    Returns:
+        str: The shuffled email address.
+    Raises:
+        ValueError: If the input email address is not valid.'''
+
+    email_parts = email.split('@')
+    username = email_parts[0]
+    domain = email_parts[1]
+    shuffled_username = ''.join(random.sample(username, len(username)))
+    shuffled_email = shuffled_username + '@' + domain
+    return shuffled_email
+
+# Define the truncation function to mask email addresses
+def mask_email(email):
+    """Mask the username part of an email address by replacing all but the first 3 characters with an ellipsis.
+    Parameters:
+        email (str): A string representing an email address in the standard format with a single '@' character separating the username and domain parts.
+    Returns:
+        str: A masked email address with the same domain part as the input, but with the username part truncated and replaced with an ellipsis.
+    Example:
+        >>> mask_email('john.doe@example.com')
+        'joh...@example.com' """
+    username, domain = email.split('@')
+     # maksing the username part to the first 3 characters and add
+    truncated_username = username[:3] + '...'
+    # Combine the masked username and domain parts to form the masked email address
+    masked_email = truncated_username + '@' + domain
+    return masked_email
+
+
+# Define the padding function to mask email addresses
+def pad_email(email):
+    """
+    Obfuscate the username part of an email address by replacing vowels and consonants with random digits.
+    Parameters:
+        email (str): A string representing an email address in the standard format with a single '@' character separating the username and domain parts.
+    Returns:
+        str: An obfuscated email address with the same domain part as the input, but with the username part replaced with random digits for each vowel or consonant character.
+    Example:
+        >>> obfuscate_email('jane.doe@example.com')
+        'j8n6.d48@ex1mpl5.com'
+    """
+    # Define a list of characters to choose
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+     # Add random characters to the email address
+    char=''
+    for i in range(5):
+        char += random.choice(chars)
+        #it chooses from the second character and do masking
+    pad_email = email[:1] + '....' + char+ email[-10:]
+    return pad_email
+
+#Define the function to mask email addresses
+def obfuscate_email(email):
+    """
+    Obfuscate the username part of an email address by replacing vowels and consonants with random digits.
+    Parameters:
+        email (str): A string representing an email address in the standard format with a single '@' character separating the username and domain parts.
+    Returns:
+        str: An obfuscated email address with the same domain part as the input, but with the username part replaced with random digits for each vowel or consonant character.
+    Example:
+        >>> obfuscate_email('jane.doe@example.com')
+        'j8n6.d48@ex1mpl5.com'
+    """
+    # Split email into username and domain
+    username, domain = email.split('@')
+    obfuscated_username = ''
+    for char in username:
+        if char.lower() in ['a', 'e', 'i', 'o', 'u','b','c','d']:
+            obfuscated_username += str(random.randint(0, 9))
+        else:
+            obfuscated_username += char
+    # Construct obfuscated email
+    obfuscated_email = obfuscated_username + '@' + domain
+    return obfuscated_email
+
+# Define the cryptography function to mask email addresses
+
+
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+fernet = Fernet(key)
+def encrypt_email(email):
+    """Encrypts the input email using Fernet encryption method.
+Args:
+    email (str): The email address to be encrypted.
+Returns:
+    bytes: The encrypted email address as bytes."""
+    encrypted_email = fernet.encrypt(email.encode())
+    return encrypted_email
+
+data['Email address'] = data['Email address'].apply(obfuscate_email)
+print(data['Email address'])
